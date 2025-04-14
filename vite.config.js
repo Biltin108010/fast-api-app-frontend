5caac5fdb09b07c266bb40d078fecec086771ea2
Biltin108010 <Villanueva.johnrhey4@gmail.com>
2025-04-14 22:12:02 +0800

    fix???

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/fast-api-frontend/', // Make sure this matches your repo name
+  base: '/fast-api-app-frontend/', // Make sure this matches your repo name
 });
 
 
