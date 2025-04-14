9c3fc6c5c012cae22a54fedea198246169fee7fd
Biltin108010 <Villanueva.johnrhey4@gmail.com>
2025-04-14 21:44:17 +0800

    initial commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_URL = "https://todo-backend-6osi.onrender.com/api/tasks/";
+const API_URL = "https://fastapi-app-iscc.onrender.com/api/tasks";
 
+// Get all tasks (with optional search)
 export const getTasks = async (search = "") => {
   try {
     const response = await axios.get(`${API_URL}?search=${search}`);
@@ -11,6 +12,7 @@ export const getTasks = async (search = "") => {
   }
 };
 
+// Add a new task
 export const addTask = async (task) => {
   try {
     const response = await axios.post(API_URL, task);
@@ -20,18 +22,20 @@ export const addTask = async (task) => {
   }
 };
 
+// Update an existing task by ID
 export const updateTask = async (id, task) => {
   try {
-    const response = await axios.put(`${API_URL}${id}/`, task);
+    const response = await axios.put(`${API_URL}/${id}`, task);
     return response.data;
   } catch (error) {
     console.error("Error updating task:", error);
   }
 };
 
+// Delete a task by ID
 export const deleteTask = async (id) => {
   try {
-    await axios.delete(`${API_URL}${id}/`);
+    await axios.delete(`${API_URL}/${id}`);
   } catch (error) {
     console.error("Error deleting task:", error);
   }
